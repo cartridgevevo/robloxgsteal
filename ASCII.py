17e989f662b12bfde4c7ca703fd562aba1644895
Mrcorbie <33270436+MrCorbie@users.noreply.github.com>
2019-12-22 01:21:49 +1300

    sexy ASCII art
    this gaming

---
+++ ASCII.py
@@ -0,0 +1,40 @@
+print('''  
+
+
+          
+
+                                                                                                       
+         
+
+                                                                                                       
+         
+
+                                                                                                       
+         
+
+                                                                                                       
+                                                                                                      
+                                                                                                       
+        GGGGGGGGGGGGG   SSSSSSSSSSSSSSS      tttt                                              lllllll 
+     GGG::::::::::::G SS:::::::::::::::S  ttt:::t                                              l:::::l 
+   GG:::::::::::::::GS:::::SSSSSS::::::S  t:::::t                                              l:::::l 
+  G:::::GGGGGGGG::::GS:::::S     SSSSSSS  t:::::t                                              l:::::l 
+ G:::::G       GGGGGGS:::::S        ttttttt:::::ttttttt        eeeeeeeeeeee    aaaaaaaaaaaaa    l::::l 
+G:::::G              S:::::S        t:::::::::::::::::t      ee::::::::::::ee  a::::::::::::a   l::::l 
+G:::::G               S::::SSSS     t:::::::::::::::::t     e::::::eeeee:::::eeaaaaaaaaa:::::a  l::::l 
+G:::::G    GGGGGGGGGG  SS::::::SSSSStttttt:::::::tttttt    e::::::e     e:::::e         a::::a  l::::l 
+G:::::G    G::::::::G    SSS::::::::SS    t:::::t          e:::::::eeeee::::::e  aaaaaaa:::::a  l::::l 
+G:::::G    GGGGG::::G       SSSSSS::::S   t:::::t          e:::::::::::::::::e aa::::::::::::a  l::::l 
+G:::::G        G::::G            S:::::S  t:::::t          e::::::eeeeeeeeeee a::::aaaa::::::a  l::::l 
+ G:::::G       G::::G            S:::::S  t:::::t    tttttte:::::::e         a::::a    a:::::a  l::::l 
+  G:::::GGGGGGGG::::GSSSSSSS     S:::::S  t::::::tttt:::::te::::::::e        a::::a    a:::::a l::::::l
+   GG:::::::::::::::GS::::::SSSSSS:::::S  tt::::::::::::::t e::::::::eeeeeeeea:::::aaaa::::::a l::::::l
+     GGG::::::GGG:::GS:::::::::::::::SS     tt:::::::::::tt  ee:::::::::::::e a::::::::::aa:::al::::::l
+        GGGGGG   GGGG SSSSSSSSSSSSSSS         ttttttttttt      eeeeeeeeeeeeee  aaaaaaaaaa  aaaallllllll
+                                                                                                       
+                                                                                                       
+                                                                                                       
+
+''')
+                                                                                                                              
+                                                                                                                                      
