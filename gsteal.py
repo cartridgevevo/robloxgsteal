983099e9a0e79589eb533aac8405dd205e720e47
cartridgevevo <30766091+cartridgevevo@users.noreply.github.com>
2021-05-05 19:46:17 +1200

    removed some comments

--- gsteal.py
+++ gsteal.py
@@ -23,7 +23,7 @@ data = json.load(open('config.json', 'r'))
 url = data['url']
 url_failed = data['url_failed']
 url_10ormode = data['url_10ormode']
-# url_100member = data['url_100member'] - Hey there, you're peeking into the future of GSteal, fuck off
+# url_100member = data['url_100member'] - Hey there, you're peeking into the future of GSteal
 
 # Robux Configuration - Connected to config.json
 lowrobux = data['lowrobux']
